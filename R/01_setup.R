da2100c775cf4c3a859fe23d5ad7364d4bf03829
Alasdair B R Stewart <alasdair.b.r.stewart@gmail.com>
2025-10-17 16:08:49 +0100

    Updated packages for script to install.

--- R/01_setup.R
+++ R/01_setup.R
@@ -3,10 +3,11 @@
 # Date: 21/09/2025
 # Author: Alasdair B R Stewart
 
-list.of.packages <- c("tidyverse", "broom", "modelsummary", "devtools")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+list.of.packages <- c("rmarkdown", "knitr", "tidyverse", "broom", "modelsummary", "devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
+if (length(new.packages)) install.packages(new.packages)
 # Wordcount addin if missing
 if (!"wordcountaddin" %in% installed.packages()[, "Package"]) {
   devtools::install_github("benmarwick/wordcountaddin", type = "source", dependencies = TRUE)
 }
+
